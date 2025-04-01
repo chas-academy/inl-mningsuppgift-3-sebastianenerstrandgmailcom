d337ab344c48898cf5ff0cd1a5db207b082d33fb
sebastianenerstrand@gmail.com <sebastianenerstrand@gmail.com>
2025-04-01 07:05:51 +0200

    Add calculate function, square x and y and return the sum squared

--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 
 // This is where you add the function calculate_hypotenuse
 
+float calculate_hypotenuse(float x, float y){
+    float x_squared = pow(x, 2);
+    float y_squared = pow(y, 2);
+
+    return sqrt(x_squared + y_squared);
+}
+
 int main() {
     float a = 3.0;
     float b = 4.0;
